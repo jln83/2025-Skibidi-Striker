41e43cdf119c03098853aa04ea41474fbfdfe0ca
clembon <clembon31@gmail.com>
2024-12-10 17:16:41 +0100

    Update mobs.py
    ajout de troupe skibidi
    modification detection des degats (coté mob)

--- mobs.py
+++ mobs.py
@@ -4,26 +4,68 @@ from random import randint
 
 
 class Mini_skibidi:
-    def __init__(self, x, y, speed):
+    def __init__(self, x, y, speed, target):
+        self.memox = x
+        self.memoy = y
         self.x = x
         self.y = y
         self.img_tete = pygame.image.load('images_de _devellopement/tete_skibidi.png').convert_alpha()
         self.img_toilettes = pygame.image.load('images_de _devellopement/toilette_sanst_tete.png').convert_alpha()
-        self.speedx = speed
-        self.speedy = speed*2
-        self.targetx = 0
-        self.targety = 0
+        self.largeur = 90
+        self.hauteur = 90
+        self.speed = speed
+        self.target_x = target[0]
+        self.target_y = target[1]
+        self.vie=100
 
     def act_img(self):
+        if self.x > self.target_x > self.memox:
+            self.memox = ecran_jeu.largeur-self.memox
         ecran_jeu.screen.blit(self.img_toilettes, (self.x, self.y))
         ecran_jeu.screen.blit(self.img_tete, (self.x, self.y))
-        normx = self.targetx-self.x
-        normy = self.targety-self.y
-        d=math.sqrt(normx**2+normy**2)
-        self.x += self.speedx*normx/d
-        self.y += self.speedy*normy/d
-        self.targetx=camera.x
-        self.targety=camera.y
+
+        dx = (self.x-self.target_x)/(self.target_x-self.memox)
+        self.x += self.speed #* abs(self.targetx-self.memox)/300 #deux version selon celle voulue enlever #
+        self.y = (self.memoy-self.target_y)*dx**2 + self.target_y
+
+    def touche(self, bullets):
+        for bullet in bullets:
+            if self.x <= bullet.x <= self.x+self.largeur and self.y <= bullet.y <= self.y+self.hauteur:
+                self.vie -= bullet.damage
+
+
+class Troupe_mini_skibidi:
+    def __init__(self, nb, x, y, speed):
+        self.troupe = []
+        self.nb = nb
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.tempo = 0
+        self.largeur = 90
+        self.hauteur = 90
+        self.target = (camera.x, camera.y)
+        self.vie = 100 * nb
+
+    def act_img(self):
+        if self.nb > 0 and self.tempo <= 0:
+            self.troupe.append(Mini_skibidi(self.x, self.y, self.speed, self.target))
+            self.nb -= 1
+            self.tempo = self.largeur/self.speed
+        i = 0
+        self.vie = 0
+        for mini_skibidi in self.troupe:
+            if mini_skibidi.x > ecran_jeu.largeur or mini_skibidi.vie <= 0:
+                self.troupe.pop(i)
+            self.vie += mini_skibidi.vie
+            mini_skibidi.act_img()
+            i += 1
+        self.tempo -= 1
+
+    def touche(self, bullets):
+        for mini_skibidi in self.troupe:
+            mini_skibidi.touche(bullets)
+
 
 class Large_skibidi:
     def __init__(self, x, y, speed):
@@ -34,6 +76,10 @@ class Large_skibidi:
         self.target_y = randint(5,200)
         self.img = pygame.image.load('images_de _devellopement/large_skibidi.png').convert_alpha()
         self.img = pygame.transform.scale(self.img,(125,125))
+        self.largeur = 125
+        self.hauteur = 125
+        self.vie = 100
+
 
     def act_img(self):
         if abs(self.x - self.target_x) > 1: #or abs(self.y - self.target_y) > 1:
@@ -53,3 +99,9 @@ class Large_skibidi:
 
         if 0 <= self.x <= ecran_jeu.largeur - 125 and 0 <= self.y <= ecran_jeu.hauteur - 125:
             ecran_jeu.screen.blit(self.img, (self.x, self.y))
+
+    def touche(self,bullets):
+        for bullet in bullets:
+            if self.x <= bullet.x <= self.x+self.largeur and self.y <= bullet.y <= self.y+self.hauteur:
+                self.vie -= bullet.damage
+
